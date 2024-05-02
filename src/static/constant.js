3defa61030e066332f7cd1c9c5595becc2c63eca
Vikash Pathak <pathakvikash821@gmail.com>
2024-05-03 02:22:34 +0530

    static assets

---
+++ src/static/constant.js
@@ -0,0 +1,8 @@
+import p1 from './p1.jpeg';
+import p2 from './p2.png';
+import p3 from './p3.png';
+import p4 from './p4.jpg';
+import p5 from './p5.png';
+import p6 from './p6.png';
+
+export { p1, p2, p3, p4, p5, p6 };
